73e245b75d00864bd3d23e1f2f313308e302b275
北京－戴旭 <daixu@jd.com>
2016-05-25 18:07:49 +0800

    Initial Commit

---
+++ Temp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Temp
+//
+//  Created by daixu on 16/5/25.
+//  Copyright © 2016年 daixu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Temp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Temp
+//
+//  Created by daixu on 16/5/25.
+//  Copyright © 2016年 daixu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
